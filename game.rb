e38cb9f4dc1b522dc34dbb2ee134684f612bc821
Justin Visser <justin@jhvisser.com>
2016-11-06 12:57:22 -0500

    Adding in the Spot objects to the spots array

--- game.rb
+++ game.rb
@@ -5,13 +5,13 @@ require 'player'
 module Fanorona
   class Game
     def initialize(playerOneName, playerTwoName)
-      @spots = Array.new(9) { Array.new(5) }
+      @spots = Array.new(9) { Array.new(5) {Spot.new} }
       @players = [Player.new(playerOneName, @spots), Player.new(playerTwoName, @spots)]
       @turn = rand(2) #TODO: Turn should start with the "white" player according to rules.
       @currentPlayer = @players[@turn]
       @rules = Rule.new(3)
 
-      #TODO: Populate spot array with Spot objects and the Piece's to go in the correct spots for game start.
+      #TODO: Put pieces in Spot's array
     end
 
     def startGame
