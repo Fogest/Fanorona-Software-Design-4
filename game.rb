7729bcbcb047b9423ba1ad2b0aaf0738cff6b060
Justin Visser <justin@jhvisser.com>
2016-11-06 10:35:09 -0500

    Adding some TODO's to the game file

--- game.rb
+++ game.rb
@@ -7,9 +7,11 @@ module Fanorona
     def initialize(playerOneName, playerTwoName)
       @spots = Array.new(9) { Array.new(5) }
       @players = [Player.new(playerOneName, @spots), Player.new(playerTwoName, @spots)]
-      @turn = rand(2)
+      @turn = rand(2) #TODO: Turn should start with the "white" player according to rules.
       @currentPlayer = @players[@turn]
       @rules = Rule.new(1)
+
+      #TODO: Populate spot array with Spot objects and the Piece's to go in the correct spots for game start.
     end
 
     def startGame
