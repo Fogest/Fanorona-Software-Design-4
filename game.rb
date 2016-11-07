f0947177f4d2802fbba40023afc8bdbb1a4bfc41
Trevor <trevor.foster@hotmail.ca>
2016-11-06 22:37:43 -0500

    Added some move validation code Fixed up a few things

--- game.rb
+++ game.rb
@@ -5,8 +5,9 @@ require 'player'
 module Fanorona
   class Game
     VALID_OPTIONS = ['move', 'exit']
+
     def initialize(playerOneName, playerTwoName)
-      @spots = Array.new(9) { Array.new(5) {Spot.new} }
+      @spots = Array.new(5) { Array.new(9) { Spot.new } }
       @players = [Player.new(playerOneName, @spots), Player.new(playerTwoName, @spots)]
       @turn = rand(2) #TODO: Turn should start with the "white" player according to rules.
       @currentPlayer = @players[@turn]
@@ -32,16 +33,47 @@ module Fanorona
             if tokens.length != 4
               puts 'Invalid input'
               next
-            elsif !%w(n ne e se s sw w).include?(tokens[3])
+            elsif !%w(n ne e se s sw w nw).include?(tokens[3])
               puts 'Invalid direction provided.'
               next
             end
 
             x, y = tokens[1].to_i, tokens[2].to_i
-            direction = tokens[3].to_sym
+            direction = tokens[3]
 
-            # Validate the move
-            # Move the piece
+            if %w(ne se sw nw).include?(direction) && (x % 2 == 0) != (y % 2 == 0)
+              puts 'You can only move diagonally at strong intersections.'
+              next
+            end
+
+            targetx, targety = x, y
+            direction.split('').each { |d|
+              dirIncr = case d
+                          when 'n'
+                            [0, -1]
+                          when 's'
+                            [0, 1]
+                          when 'e'
+                            [1, 0]
+                          when 'w'
+                            [-1, 0]
+                          else
+                            [0, 0]
+                        end
+              targetx += dirIncr[0]
+              targety += dirIncr[1]
+            }
+
+            if targetx < 0 || targetx > 4 || targety < 0 || targety > 8
+              puts 'Invalid move.'
+              next
+            end
+
+
+          if @rules.checkMoveValidity(@currentPlayer, @spots[x][y], @spots[targetx][targety])
+            @currentPlayer.makeMove(@spots[x][y], @spots[targetx][targety])
+          end
+          # Move the piece
           when :exit
             puts 'Game is exiting.'
             exit
